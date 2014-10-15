120e403b2886d4ba421cbda794d3e312e345a6b7
SUMDude21 <roosterg21@gmail.com>
2014-10-14 21:08:19 -0500

    Should be final commit.

--- project_2/stereo.py
+++ project_2/stereo.py
@@ -5,7 +5,8 @@ In this project, you'll extract dense 3D information from stereo image pairs.
 
 import cv2
 import math
-import numpy
+import numpy as np
+import StringIO
 
 
 def rectify_pair(image_left, image_right, viz=False):
@@ -19,7 +20,41 @@ def rectify_pair(image_left, image_right, viz=False):
       H_left, H_right: homographies that warp the left and right image so
         their epipolar lines are corresponding rows.
     """
-    pass
+
+    # Initiate SIFT detector
+    sift = cv2.SIFT()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(image_left, None)
+    kp2, des2 = sift.detectAndCompute(image_right, None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # store all the good matches as per Lowe's ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < 0.68 * n.distance:
+            good.append(m)
+
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good])
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good])
+
+    # find the fundamental matrix
+    F, mask = cv2.findFundamentalMat(src_pts, dst_pts, cv2.RANSAC, 3, 0.99)
+    src_pts = src_pts.flatten()
+    dst_pts = dst_pts.flatten()
+
+    # rectify the images, produce the homographies: H_left and H_right
+    retval, H_left, H_right = cv2.stereoRectifyUncalibrated(
+        src_pts, dst_pts, F, image_left.shape[:2])
+
+    return F, H_left, H_right
 
 
 def disparity_map(image_left, image_right):
@@ -32,7 +67,41 @@ def disparity_map(image_left, image_right):
       an single-channel image containing disparities in pixels,
         with respect to image_left's input pixels.
     """
-    pass
+
+    window_size = 3
+    min_disp = 16
+    num_disp = 112 - min_disp
+    stereo = cv2.StereoSGBM(minDisparity=min_disp,
+                            numDisparities=num_disp,
+                            SADWindowSize=window_size,
+                            uniquenessRatio=10,
+                            speckleWindowSize=100,
+                            speckleRange=32,
+                            disp12MaxDiff=1,
+                            preFilterCap=20,
+                            P1=8 * 3 * (window_size ** 2),
+                            P2=32 * 3 * (window_size ** 2),
+                            fullDP=False
+                            )
+
+    # compute the disparity and convert the 16bit disparity into 8 bit
+    disp = stereo.compute(image_left, image_right) / 16
+    disp = disp.astype('uint8')
+
+    return disp
+
+# PLY file header
+ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
 
 
 def point_cloud(disparity_image, image_left, focal_length):
@@ -48,4 +117,23 @@ def point_cloud(disparity_image, image_left, focal_length):
         pixels, with colors sampled from left_image. You may filter low-
         disparity pixels or noise pixels if you choose.
     """
-    pass
+    h, w = image_left.shape[:2]
+    Q = np.float32([[1, 0,  0, w / 2],
+                    [0, -1,  0,  h / 2],  # turn points 180 deg around x-axis,
+                    [0, 0, focal_length,  0],  # so that y-axis looks up
+                    [0, 0,  0,  1]])
+
+    # reproject image points to 3D space, compute the colors
+    points = cv2.reprojectImageTo3D(disparity_image, Q)
+    colors = cv2.cvtColor(image_left, cv2.COLOR_BGR2RGB)
+    mask = disparity_image > disparity_image.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+
+    # write PLY string data to StringIO object and return the contents
+    cloud = StringIO.StringIO()
+    verts = np.hstack([out_points, out_colors])
+    cloud.write(ply_header % dict(vert_num=len(verts)))
+    np.savetxt(cloud, verts, '%f %f %f %d %d %d')
+
+    return cloud.getvalue()
